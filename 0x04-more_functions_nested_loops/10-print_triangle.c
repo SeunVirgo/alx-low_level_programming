9d99d57c5422fa594811cd2694f85441e45d819b
SeunVirgo <seun.virgoe@gmail.com>
2022-09-15 15:06:44 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
 		{
-			for (inc1 = 1; inc1 <= size; inc1++)
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
 				putchar(' ');
 			}
@@ -24,7 +24,7 @@ void print_triangle(int size)
 			{
 				putchar('#');
 			}
-
+	
 			if (inc1 == size)
 			{
 				continue;
